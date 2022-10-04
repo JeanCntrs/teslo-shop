d7066720f59ab5eaed274fe93c49f52e12408240
Jean C. Contreras <jean.cntrs@gmail.com>
2022-10-04 11:00:09 -0300

    Added directory structure and shop layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,14 @@
+import { Typography } from '@mui/material'
 import type { NextPage } from 'next'
+import { ShopLayout } from '../components/layouts'
+
 
 const Home: NextPage = () => {
     return (
-        <h1>Teslo-shop</h1>
+        <ShopLayout title='Teslo-Shop - Home' pageDescription='Find the best teslo products here'>
+            <Typography variant='h1' component='h1'>Store</Typography>
+            <Typography variant='h2' sx={{mb:1}}>All the products</Typography>
+        </ShopLayout>
     )
 }
 
