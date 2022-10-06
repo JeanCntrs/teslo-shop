3274b7e6fa2c4585b8f9cb1a90523ab56fa2ae5c
Jean C. Contreras <jean.cntrs@gmail.com>
2022-10-06 18:26:47 -0300

    Added temporal home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
-import { Typography } from '@mui/material'
+import { Card, CardActionArea, CardMedia, Grid, Typography } from '@mui/material'
 import type { NextPage } from 'next'
 import { ShopLayout } from '../components/layouts'
+import { initialData } from '../database/products'
 
 
 const Home: NextPage = () => {
     return (
         <ShopLayout title='Teslo-Shop - Home' pageDescription='Find the best teslo products here'>
             <Typography variant='h1' component='h1'>Store</Typography>
-            <Typography variant='h2' sx={{mb:1}}>All the products</Typography>
+            <Typography variant='h2' sx={{ mb: 1 }}>All the products</Typography>
+
+            <Grid container spacing={4}>
+                {
+                    initialData.products.map(product => (
+                        <Grid item xs={6} sm={4} key={product.slug}>
+                            <Card>
+                                <CardActionArea>
+                                    <CardMedia 
+                                        component='img'
+                                        image={`products/${product.images[0]}`}
+                                        alt={product.title}
+                                    />
+                                </CardActionArea>
+                            </Card>
+                        </Grid>
+                    ))
+                }
+            </Grid>
         </ShopLayout>
     )
 }
