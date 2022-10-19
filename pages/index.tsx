79534ff9f459c2120b56419fba009246d9736d71
Jean C. Contreras <jean.cntrs@gmail.com>
2022-10-19 08:25:37 -0300

    Added products hooks and swr config

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,19 +3,27 @@ import { Typography } from '@mui/material'
 import { ShopLayout } from '../components/layouts'
 import { ProductList } from '../components/products'
 import { initialData } from '../database/products'
+import { useProducts } from '../hooks'
 
 
-const Home: NextPage = () => {
+
+const HomePage: NextPage = () => {
+
+
+    const { products, isLoading } = useProducts('/products');
+
     return (
         <ShopLayout title='Teslo-Shop - Home' pageDescription='Find the best teslo products here'>
             <Typography variant='h1' component='h1'>Store</Typography>
             <Typography variant='h2' sx={{ mb: 1 }}>All the products</Typography>
 
-            <ProductList
-                products={initialData.products as any}
-            />
+            {
+                isLoading
+                    ? <h1>Loading...</h1>
+                    : <ProductList products={products} />
+            }
         </ShopLayout>
     )
 }
 
-export default Home
+export default HomePage
