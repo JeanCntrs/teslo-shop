0e2291ac64a60cca9b8c481a70c7b31b10a7bafc
Jean C. Contreras <jean.cntrs@gmail.com>
2022-12-18 20:30:25 -0300

    Added order confirmation with paypal

--- pages/api/orders/pay.ts
+++ pages/api/orders/pay.ts
@@ -1,7 +1,8 @@
+import axios from 'axios';
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { getSession } from 'next-auth/react';
 import { db } from '../../../database';
-import { IOrder } from '../../../interfaces';
+import { IOrder, IPaypal } from '../../../interfaces';
 import { Order, Product } from '../../../models';
 
 type Data = { message: string }
@@ -17,26 +18,72 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
     }
 }
 
-const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    const { orderItems, total } = req.body as IOrder;
+const getPaypalBearerToken = async (): Promise<string | null> => {
+    const PAYPAL_CLIENT = process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID;
+    const PAYPAL_SECRET = process.env.PAYPAL_SECRET;
 
-    const session: any = await getSession({ req });
-
-    if (!session) {
-        return res.status(4001).json({ message: 'Unauthorized' });
-    }
-
-    const productsIds = orderItems.map(product => product._id);
-
-    await db.connect();
-
-    const dbProducts = await Product.find({ _id: { $in: productsIds } });
+    const base64Token = Buffer.from(`${PAYPAL_CLIENT}:${PAYPAL_SECRET}`, 'utf-8').toString('base64');
+    const body = new URLSearchParams('grant_type=client_credentials')
 
     try {
+        const { data } = await axios.post(process.env.PAYPAL_OAUTH_URL || '', body, {
+            headers: {
+                'Authorization': `Basic ${base64Token}`,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
 
-    } catch (error: any) {
-        await db.disconnect();
-        console.log('error in payOrder', error)
-        return res.status(400).json({ message: error.message || 'Check server logs' });
+        return data.access_token
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.log(error.response?.data);
+        } else {
+            console.log(error);
+        }
+
+        return null;
     }
+}
+
+const payOrder = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const paypalBearerToken = await getPaypalBearerToken();
+
+    if (!paypalBearerToken) {
+        return res.status(400).json({ message: 'Token not created' });
+    }
+
+    const { transactionId = '', orderId = '' } = req.body;
+
+    const { data } = await axios.get<IPaypal.PaypalOrderStatusResponse>(`${process.env.PAYPAL_ORDERS_URL}/${transactionId}`, {
+        headers: {
+            'Authorization': `Bearer ${paypalBearerToken}`
+        }
+    });
+
+    if (data.status !== 'COMPLETED') {
+        return res.status(401).json({ message: 'Order not completed' });
+    }
+
+    await db.connect();
+    const dborder = await Order.findById(orderId);
+
+    if (!dborder) {
+        await db.disconnect();
+
+        return res.status(400).json({ message: 'Order not found' });
+    }
+
+    if (dborder.total !== Number(data.purchase_units[0].amount.value)) {
+        await db.disconnect();
+
+        return res.status(400).json({ message: 'Invalid amount' });
+    }
+
+    dborder.transactionId = transactionId;
+    dborder.isPaid = true;
+    await dborder.save();
+
+    await db.disconnect();
+
+    return res.status(200).json({ message: 'Paid order' });
 }
