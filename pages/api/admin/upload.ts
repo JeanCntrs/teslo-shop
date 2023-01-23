c0107b9302623b393dd55d74bb8be3cb06eed3c7
Jean C. Contreras <jean.cntrs@gmail.com>
2023-01-22 22:08:00 -0300

    final commit

--- pages/api/admin/upload.ts
+++ pages/api/admin/upload.ts
@@ -1,6 +1,10 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import formidable from 'formidable';
 
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config(process.env.CLOUDINARY_URL || '');
+
 type Data =
     | { message: string }
 
@@ -20,12 +24,28 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
     }
 }
 
-const uploadFile = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-    await parseFiles(req);
+const parseFiles = async (req: NextApiRequest): Promise<string> => {
+    return new Promise((resolve, reject) => {
+        const form = new formidable.IncomingForm();
+        form.parse(req, async (err, fields, files) => {
+            if (err) {
+                return reject(err);
+            }
 
-    return res.status(200).json({ message: 'ok' });
+            const filePath = await saveFile(files.file as formidable.File);
+            resolve(filePath);
+        })
+    });
 }
 
-const parseFiles = async (req: NextApiRequest) => {
+const saveFile = async (file: formidable.File): Promise<string> => {
+    const { secure_url } = await cloudinary.uploader.upload(file.filepath);
 
+    return secure_url;
+}
+
+const uploadFile = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const imageUrl = await parseFiles(req);
+
+    return res.status(200).json({ message: imageUrl });
 }
